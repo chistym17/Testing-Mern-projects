2b0a6a70cc74ce93d783c35240788579d87436c7
chisty <mchisty31@gmail.com>
2024-05-24 19:20:28 +0600

    database mocking tested

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,37 @@
+
 import express from "express";
+import { z } from "zod";
+import { prismaClient } from "./__mocks__/db";
 
 export const app = express();
 app.use(express.json());
 
-app.post("/sum", (req, res) => {
-    const a = req.body.a;
-    const b = req.body.b;
-    const answer = a + b;
+const sumInput = z.object({
+    a: z.number(),
+    b: z.number()
+})
 
+app.post("/sum", async (req, res) => {
+    const parsedResponse = sumInput.safeParse(req.body)
+
+    if (!parsedResponse.success) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
+
+
+    const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+    await prismaClient.sum.create({
+        data: {
+            a: parsedResponse.data.a,
+            b: parsedResponse.data.b,
+            result: answer
+        }
+    })
     res.json({
         answer
     })
 });
+
